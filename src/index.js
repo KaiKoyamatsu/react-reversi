c2563c2e2d452aedface230a7a6cdd3c5c29edee
seakun <c011514454@edu.teu.ac.jp>
2018-03-04 19:15:00 +0900

    Add オセロの台を作成

--- src/index.js
+++ src/index.js
@@ -2,13 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'
 
-function Test (props) {
+function Block (props) {
   return (
-    <h1>Hello, world!</h1>
+    <div></div>
   );
 }
 
+//複数のBlockを宣言する関数コンポーネント
+function BlockList (props) {
+  const blockArray = props.blockArray;
+  const blocks = blockArray.map((value, index) => <Block key={index} />);
+  return <div className='block'>{blocks}</div>;
+}
+
+class Reversi extends React.Component {
+  constructor(props) {
+    super(props);
+
+    /*
+    blockArray: Blockを宣言する配列
+    */
+    this.state = {
+      blockArray: Array(64).fill(null),
+    };
+  }
+
+  render() {
+    return (
+      <div className='field'>
+        <BlockList blockArray = {this.state.blockArray} />
+      </div>
+    );
+  }
+}
+
+/* ------------------------------------------------------ */
+
 ReactDOM.render(
-  <Test />,
+  <Reversi />,
   document.getElementById('root')
 );
