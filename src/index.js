d44d07f051f441faf3564544126d3b0e38c4bb23
seakun <c011514454@edu.teu.ac.jp>
2018-03-04 17:05:48 +0900

    Update hello_appをtestコンポーネントでレンダリング

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'
 
+function Test (props) {
+  return (
+    <h1>Hello, world!</h1>
+  );
+}
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <Test />,
   document.getElementById('root')
 );
