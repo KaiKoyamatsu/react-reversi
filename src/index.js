6f9ab69f1d0a6b0232577da170a073547cd4ea7a
seakun <c011514454@edu.teu.ac.jp>
2018-03-07 23:14:52 +0900

    Update 右側のマスをひっくり返す関数を追加 　　　 入力されているマスをクリックした場合に処理を中断するように変更

--- src/index.js
+++ src/index.js
@@ -36,17 +36,48 @@ class Reversi extends React.Component {
     return <div className='block'>{blocks}</div>;
   }
 
+  //マスをクリックした時の動作
   handleClick(i) {
     const blockArray = this.state.blockArray.slice();
-    blockArray[i] = this.state.whiteIsNext ? '⚪️' : '⚫️';
+
+    if (blockArray[i]) {
+      return;
+    }
+
+    blockArray[i] = this.state.whiteIsNext ? 'O' : 'X';
+    this.turnOverRight(blockArray, i);
     this.setState({
       blockArray: blockArray,
       whiteIsNext: !this.state.whiteIsNext,
     });
   }
 
-  turnOver(blockArray, i) {
+  //右のコマをひっくり返す
+  turnOverRight(blockArray, currentPosistion) {
+    //列の中で一番低い値を計算
+    const colMinValue = currentPosistion%8;
 
+    //コマを置いた位置の右の空白を計算
+    const rightSpace = 7-colMinValue;
+
+    for (let shift = 1; shift <= rightSpace; shift++) {
+      //一つ右が同じ色か、空白があったらreturn
+      if (
+        (shift === 1 && blockArray[currentPosistion] === blockArray[currentPosistion+shift])
+        || blockArray[currentPosistion+shift] === null
+      ) {
+        return;
+      }
+
+      //二つ以上右が同じ色だと間をひっくり返す
+      if(blockArray[currentPosistion] === blockArray[currentPosistion+shift]) {
+        for (let shiftTurnPosition = 1; shiftTurnPosition < shift; shiftTurnPosition++) {
+          blockArray[currentPosistion+shiftTurnPosition] = blockArray[currentPosistion];
+        }
+        this.setState({blockArray: blockArray});
+        return
+      }
+    }
   }
 
   render() {
