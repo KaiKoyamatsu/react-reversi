e4449c18927d68b8f246ad45c2b184e296113b01
seakun <c011514454@edu.teu.ac.jp>
2018-03-26 17:20:48 +0900

    ガラッと変えるからとりまプッシュ

--- src/index.js
+++ src/index.js
@@ -25,34 +25,116 @@ class Reversi extends React.Component {
 
   //複数のBlockを宣言する関数コンポーネント
   blockList() {
-    const blockArray = this.state.blockArray;
-    const blocks = blockArray.map((value, index) =>
-      value.map((x, y) =>
+    console.log("aaa");
+    console.log("check", this.state.blockArray);
+    const blockArray = this.state.blockArray.slice();
+    const blocks = blockArray.map((rowArray, row) =>
+      rowArray.map((colArray, col) =>
         <Block
-          key={index + y}
-          value={blockArray[index][y]}
-          onClick={() => this.handleClick(index, y)}
+          key={row + ':' + col}
+          value={blockArray[row][col]}
+          onClick={() => this.handleClick(row, col)}
         />
       )
     );
+    console.log("return");
 
     return <div className='block'>{blocks}</div>;
   }
 
   // マスをクリックした時の動作
-  // handleClick(i) {
-  //   const blockArray = this.state.blockArray.slice();
-  //
-  //   if (blockArray[i]) {
-  //     return;
-  //   }
-  //
-  //   blockArray[i] = this.state.whiteIsNext ? '⚪️' : '⚫️';
-  //   this.setState({
-  //     blockArray: blockArray,
-  //     whiteIsNext: !this.state.whiteIsNext,
-  //   });
-  // }
+  handleClick(row, col) {
+    const blockArray = this.state.blockArray.slice();
+
+    if (blockArray[row][col]) return;
+
+    //多次元配列は下の次元のオブジェクトが参照渡しになってしまうため、わざわざ配列を宣言している
+    const childBlockArray = blockArray[row].slice();
+    childBlockArray[col] = this.state.whiteIsNext ? '⚪️' : '⚫️';
+    blockArray[row] = childBlockArray;
+
+    blockArray[row] = this.searchRightSide(blockArray, row, col);
+    console.log(this.searchRightSide(blockArray, row, col)) ;
+    // this.searchRightSide(blockArray, row, col);
+    // this.searchLeftSide(blockArray, row, col);
+    // this.searchUpSide(blockArray, row, col);
+
+    // this.setState({
+    //   blockArray: blockArray,
+    //   whiteIsNext: !this.state.whiteIsNext,
+    // });
+  }
+
+  /*
+  探索の際に、わざわざ値渡しを頑張っているのは、探索を一回にするため
+  */
+
+  // コマが置かれた右側のマスを探索していく
+  searchRightSide(blockArray, row, col) {
+    const childBlockArray = [...blockArray[row]];
+
+    //一つ右が相手のコマじゃなければreturn
+    if(childBlockArray[col+1] === childBlockArray[col] || childBlockArray[col+1] == null) return blockArray[row];
+
+    childBlockArray[col+1] = childBlockArray[col];
+
+    for(let i = col+2; i < childBlockArray.length; i++){
+      if (childBlockArray[i] == null) break;
+      if (childBlockArray[col] === childBlockArray[i]) return childBlockArray;
+      childBlockArray[i] = childBlockArray[col];
+    }
+    return blockArray[row];
+  }
+
+  // コマが置かれた右側のマスを探索していく
+  searchLeftSide(blockArray, row, col) {
+    const childBlockArray = blockArray[row].slice();
+
+    //一つ左が相手のコマじゃなければreturn
+    if(childBlockArray[col-1] === childBlockArray[col] || childBlockArray[col-1] == null) return;
+
+    childBlockArray[col-1] = childBlockArray[col];
+
+    for(let i = col-2; i >= 0; i--) {
+      if (childBlockArray[i] == null) break;
+      if (childBlockArray[col] === childBlockArray[i]) {
+        blockArray[row] = childBlockArray;
+        break;
+      }
+      childBlockArray[i] = childBlockArray[col];
+    }
+    return;
+  }
+
+  //blockArrayCopyがなんとか値渡しできるように頑張ってる
+  byValueRow(blockArrayCopy, currentRow, shiftedRow, col) {
+    const childBlockArray = blockArrayCopy[shiftedRow].slice();
+    childBlockArray[col] = blockArrayCopy[currentRow][col];
+    blockArrayCopy[shiftedRow] = childBlockArray;
+  }
+
+  // コマが置かれた右側のマスを探索していく
+  searchUpSide(blockArray, row, col) {
+    const blockArrayCopy = blockArray.slice();
+
+    //1行目か0か一つ上が相手のコマじゃなければreturn
+    if (row === 0) return;
+    if(blockArrayCopy[row-1][col] === blockArrayCopy[row][col] || blockArrayCopy[row-1][col] == null) return;
+
+    this.byValueRow(blockArrayCopy, row, row-1, col);
+
+    for(let i = row-2; i >= 0; i--){
+      if (blockArrayCopy[i][col] == null) break;
+      if (blockArrayCopy[row][col] === blockArrayCopy[i][col]){
+        console.log(blockArrayCopy);
+
+        break;
+      }
+
+      this.byValueRow(blockArrayCopy, row, i, col);
+    }
+    return;
+  }
 
   render() {
     return (
